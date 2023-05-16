2d645480748af843e99830636f3213d3b5604787
Kareem <kareemelnaghy@aucegypt.edu>
2023-05-16 23:07:57 +0300

    Initial Commit

---
+++ Hidden Lab 10/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hidden Lab 10
+//
+//  Created by Kareem Elnaghy on 5/16/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
